60b8a632b90a9fa48b3b590c516ed2d6db87f48f
sciencebuff <lmbjas002@myuct.ac.za>
2017-02-09 17:26:06 +0100

    Lots of updates, a working "RNA_process" file that aligns reads (SE or PE) to a genome using either STAR or Bowtie2 depending on the organism. It can then align the reads that did not align to the first genome to a second genome. This allows for the mixed reads from complex samples to be seperated. Further testing underway. Just trying to see how to make bash scripts nice but will probably use pyhton in future.

--- HumanGenome/GenomeRef/H38.3_RNAseq_pipeline.sh
+++ HumanGenome/GenomeRef/H38.3_RNAseq_pipeline.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+#more than one alignemnt from same sample fastq file?
+
+#
+
+
+
+
+
+
+
+
 Script_dir=$(dirname "$0")
 read_file="${1}"
 threads="$2"
 ram=$(expr $threads \* 2)
 indir=$(dirname "$1")
-
 name=$(basename "$1")
 out_dir=${indir}/${name%_*}
 mkdir "$out_dir"
@@ -15,14 +26,14 @@ Ref_bowtie="Script_dir/refs/GCF_000001405.34_GRCh38.p8_genomic"
 cd "$indir"
 
 #FastQC
-/home/lmbjas002/bin/FastQC/fastqc "$read_file" -o "$out_dir"
+fastqc "$read_file" -o "$out_dir"
 
 #Trim Reads
-java -Xmx"$ram"g -jar ~/bin/Trimmomatic-0.36/trimmomatic.jar SE -phred33 \
+java -Xmx"$ram"g -jar Trimmomatic-0.36/trimmomatic.jar SE -phred33 \
   "$read_file" \
   "${read_file}.trimmed.fq.gz" \
-  ILLUMINACLIP:"./bin/programs/Trimmomatic-0.36/adapters/TruSeq3-PE.fa":2:30:10 LEADING:2 TRAILING:2 SLIDINGWINDOW:4:10 MINLEN:20
-#fix adapter
+  ILLUMINACLIP:"Trimmomatic-0.36/TruSeq3-PE.fa":2:30:10 LEADING:2 TRAILING:2 SLIDINGWINDOW:4:10 MINLEN:20
+
 
 mv "${read_file}.trimmed.fq.gz" "$out_dir"
 cd "$out_dir"
@@ -36,17 +47,13 @@ cd "/home/lmbjas002/RNA_pipeline/human/pipeline/GenomeRef/refs"
 
 
 #Align Reads (Tophat 2)
-export PATH=/opt/exp_soft/bowtie2-2.2.6/:$PATH
-export PATH=/opt/exp_soft/samtools-1.2/:$PATH
-
-/home/lmbjas002/bin/tophat-2.1.1.Linux_x86_64/tophat2 -o "$out_dir" -p $threads --no-coverage-search  $Ref_bowtie "$read_file"
-# /opt/exp_soft/tophat-2.0.10/tophat2 -o "$out_dir" -p $threads --no-coverage-search  $Ref_bowtie "$read_file"
+tophat2 -o "$out_dir" -p $threads --no-coverage-search  "$Ref_bowtie" "$read_file"
 
 #Index using samtools
-/opt/exp_soft/samtools-1.2/samtools index "accepted_hits.bam"
+samtools index "accepted_hits.bam"
 
 #Mark PCR duplicates with PICARD
-java -Xmx"$ram"g -jar ~/bin/picard/picard.jar MarkDuplicates \
+java -Xmx"$ram"g -jar picard.jar MarkDuplicates \
         INPUT="accepted_hits.bam" \
         OUTPUT="accepted_hits.sorted.dedup.bam" \
         VALIDATION_STRINGENCY=LENIENT \
@@ -55,15 +62,34 @@ java -Xmx"$ram"g -jar ~/bin/picard/picard.jar MarkDuplicates \
         M="${read_file}.dedup.bam.txt"
 
 #Cufflinks
-/opt/exp_soft/cufflinks-2.1.1/cufflinks -q -p $threads -m 50 -b $Ref_name -u -o "$outdir" -g $Ref_gtf "accepted_hits.sorted.dedup.bam"
+cufflinks -q -p $threads -m 50 -b $Ref_name -u -o "$outdir" -g $Ref_gtf "accepted_hits.sorted.dedup.bam"
 
 #CuffQuant to ref
-/opt/exp_soft/cufflinks-2.2.1/cuffquant -p $threads $Ref_gtf "accepted_hits.sorted.dedup.bam"
+cuffquant -p $threads $Ref_gtf "accepted_hits.sorted.dedup.bam"
 
 #raw counts
-/home/lmbjas002/bin/HTSeq/scripts/htseq-count -f bam "accepted_hits.sorted.dedup.bam" $Ref_gtf > "${outdir}/${name}.HTSeq.counts"
+htseq-count -f bam "accepted_hits.sorted.dedup.bam" $Ref_gtf > "${outdir}/${name}.HTSeq.counts"
 
 #rename
 mv "accepted_hits.sorted.dedup.bam" "${name}_accepted_hits.sorted.dedup.bam"
+
+#rename files
+mv abundances.cxb "${name}.abundances.cxb"
+mv accepted_hits.sorted.dedup.bam.gz "${name}.accepted_hits.sorted.dedup.bam.gz"
+mv align_summary.txt "${name}.align_summary.txt"
+mv deletions.bed "${name}.deletions.bed"
+mv genes.fpkm_tracking "${name}.genes.fpkm_tracking"
+mv insertions.bed "${name}.insertions.bed"
+mv isoforms.fpkm_tracking "${name}.isoforms.fpkm_tracking"
+mv junctions.bed "${name}.junctions.bed"
+mv prep_reads.info "${name}.prep_reads.info"
+mv skipped.gtf "${name}.skipped.gtf"
+mv transcripts.gtf "${name}.transcripts.gtf"
+mv unmapped.bam.gz "${name}.unmapped.bam.gz"
+
+#delete files
+rm *trimmed*
+rm accepted_hits.bam
+rm accepted_hits.bam.bai
 
 
